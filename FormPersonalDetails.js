6e70ede02490ef181271525ac2aea3e3618a79fb
keerti2002 <101347333+keerti2002@users.noreply.github.com>
2022-06-08 15:00:57 +0530

    Add files via upload

---
+++ FormPersonalDetails.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+export class FormPersonalDetails extends Component {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  };
+
+  back = e => {
+    e.preventDefault();
+    this.props.prevStep();
+  };
+
+  render() {
+    const { values, handleChange } = this.props;
+    return (
+      <MuiThemeProvider>
+        <>
+          <Dialog
+            open
+            fullWidth
+            maxWidth='sm'
+          >
+            <AppBar title="Enter Personal Details" />
+            <TextField
+              placeholder="Enter Your USN"
+              label="USN"
+              onChange={handleChange('USN')}
+              defaultValue={values.USN}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your Department"
+              label="Department"
+              onChange={handleChange('Department')}
+              defaultValue={values.Department}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+            <TextField
+              placeholder="Enter Your Interested Domain"
+              label="InterestedDomain"
+              onChange={handleChange('InterestedDomain')}
+              defaultValue={values.InterestedDomain}
+              margin="normal"
+              fullWidth
+            />
+            <br />
+
+            <Button
+              color="secondary"
+              variant="contained"
+              onClick={this.back}
+            >previous</Button>
+
+            <Button
+              color="primary"
+              variant="contained"
+              onClick={this.continue}
+            >Next</Button>
+          </Dialog>
+        </>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default FormPersonalDetails;
